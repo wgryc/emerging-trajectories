80f016bcd47e50e37179d2bc79fc3ed40d00402b
Wojciech Gryc <wojciech@gmail.com>
2024-01-20 01:26:57 -0500

    Adding support for API calls.

--- emergingtrajectories/__init__.py
+++ emergingtrajectories/__init__.py
@@ -1,2 +1,47 @@
+import requests
+import json
+
 def hello():
-    print("Hi there! This is a placeholder.")
+    print("Welcome to the Emerging Trajectories package! We've been expecting you. 😉")
+
+class Client(object):
+
+    base_url = "https://emergingtrajectories.com/a/api/v0.2/"
+
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    def create_statement(self, title, description, deadline):
+        url = self.base_url + "create_statement"
+        headers = {
+            'Authorization': f'Bearer {self.api_key}',
+            'Content-Type': 'application/json'
+        }
+        data = {
+            "title": title,
+            "description": description,
+            "deadline": deadline
+        }
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+        if response.status_code == 201:
+            return response.json()
+        else:
+            raise Exception(response.text)
+        
+    def create_forecast(self, statement_id, title, justification, value, prediction_agent):
+        url = self.base_url + "create_forecast/" + str(statement_id)
+        headers = {
+            'Authorization': f'Bearer {self.api_key}',
+            'Content-Type': 'application/json'
+        }
+        data = {
+            "title": title,
+            "justification": justification,
+            "value": value,
+            "prediction_agent": prediction_agent
+        }
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+        if response.status_code == 201:
+            return response.json()
+        else:
+            raise Exception(response.text)
---
+++ test.py
@@ -0,0 +1,11 @@
+from emergingtrajectories import Client 
+
+c = Client(api_key = "sk-et-eegIKTiiTep9mCeA68y46ev1UDaA0W8tZHx0IQ0Xmu5zv7XxdsG0ou2tXFJxvxZXpiA")
+
+#response = c.create_statement(title = "The price of Bitcoin will exceed $100,000 by the end of 2024.",
+#                   description = "Same as above.",
+#                   deadline = "2024-12-31T23:59:59Z")
+#print(response)
+
+#response = c.create_forecast(1, "Test", "Test", 0.1, "Agent 1")
+#print(response)
