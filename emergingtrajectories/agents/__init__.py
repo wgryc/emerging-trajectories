92b047a8fea8988d1b3737a1ebd9567ec046b548
Wojciech Gryc <wojciech@gmail.com>
2024-02-06 02:05:28 -0500

    init.py fixes

--- emergingtrajectories/agents/__init__.py
+++ emergingtrajectories/agents/__init__.py
@@ -1,5 +1,2 @@
 from .scrapeandpredict import *
-from .scrapeandpredict2 import ScrapeAndPredictAgent2
-from .scrapeandpredict3 import ScrapeAndPredictAgent3
 from .extendforecast import *
-from .extendforecast2 import ExtendScrapePredictAgent2
