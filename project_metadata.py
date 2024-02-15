6941b56691b2aa39a69d8cdc3b07610be43ae1db
Wojciech Gryc <wojciech@gmail.com>
2024-02-15 18:49:48 -0500

    Preparing for merge.

--- project_metadata.py
+++ project_metadata.py
@@ -2,7 +2,7 @@ NAME = "emergingtrajectories"
 
 AUTHOR = "Wojciech Gryc"
 
-VERSION = "0.2.3"
+VERSION = "0.2.4"
 
 DESCRIPTION = "Open source library for tracking and saving forecasts of political, economic, and social events."
 
