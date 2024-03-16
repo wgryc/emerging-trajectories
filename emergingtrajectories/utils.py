1be2f4b09d39b6e059376c82ea70053d91dfdf85
Wojciech Gryc <wojciech@gmail.com>
2024-03-15 22:40:05 -0400

    Minor bug fixes.

--- emergingtrajectories/utils.py
+++ emergingtrajectories/utils.py
@@ -31,6 +31,7 @@ On the extremely rare occasion that the TEXT does not have a proper numerical pr
 # Error message used when the prediction cannot be extracted from the response.
 _extract_prediction_prompt_error = "UNCLEAR"
 
+
 def is_numeric(string: str) -> bool:
     """
     Checks whether the 'string' passed as an argument can be converted into a numeric value.
@@ -49,23 +50,27 @@ def is_numeric(string: str) -> bool:
     except ValueError:
         return False
 
+
 # TODO document
 def run_forecast(function_to_call, n, *args, **kwargs):
 
     if n == 0:
         return None
 
-    result = None 
+    result = None
 
     try:
         result = function_to_call(*args, **kwargs)
     except Exception as e:
+        print(f"Forecast failed with error: {e}")
+        print(f"Trying up to {n-1} more times.")
         result = run_forecast(function_to_call, n - 1, *args, **kwargs)
 
     if result is None:
         print(f"Forecast failed after {n} attempts.")
 
-    return result 
+    return result
+
 
 class UtilityHelper(object):
 
