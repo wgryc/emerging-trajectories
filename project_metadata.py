b7e746d7b5eccfcb6d86fe61734037a3db42118c
Wojciech Gryc <wojciech@gmail.com>
2024-04-20 23:25:13 -0400

    Removing FACTS and SOURCES from FactsRAG

--- project_metadata.py
+++ project_metadata.py
@@ -2,7 +2,7 @@ NAME = "emergingtrajectories"
 
 AUTHOR = "Wojciech Gryc"
 
-VERSION = "0.2.20"
+VERSION = "0.2.21"
 
 DESCRIPTION = "Open source library for tracking and saving forecasts of political, economic, and social events."
 
